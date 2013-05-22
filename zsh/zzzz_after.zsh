4a0f02e5143ea31ba7c254fa2b2079b8fe171713
Mark Miyashita <markm@yelp.com>
2013-05-22 16:36:11 -0700

    ssh-agent for tmux and zsh

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -4,3 +4,13 @@ if [ -d $HOME/.zsh.after/ ]; then
     for config_file ($HOME/.zsh.after/*.zsh) source $config_file
   fi
 fi
+
+# Predictable SSH authentication socket location.
+SOCK="/tmp/ssh-agent-$USER-screen"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+  rm -f /tmp/ssh-agent-$USER-screen
+  ln -sf $SSH_AUTH_SOCK $SOCK
+  export SSH_AUTH_SOCK=$SOCK
+fi
+
