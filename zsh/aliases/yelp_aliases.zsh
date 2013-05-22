8992d599abdfc9db1fe4892327017131032d6f22
Mark Miyashita <markm@yelp.com>
2013-05-22 16:38:00 -0700

    conflicting aliases

--- zsh/aliases/yelp_aliases.zsh
+++ zsh/aliases/yelp_aliases.zsh
@@ -1,5 +1,5 @@
 alias yelp="cd ~/pg/yelp-main"
-alias test="cd ~/pg/yelp-main/yelp/tests"
+alias ytest="cd ~/pg/yelp-main/yelp/tests"
 alias api="cd ~/pg/yelp-main/api_cmds"
 alias api-test="cd ~/pg/yelp-main/yelp/tests/api_cmds"
 
