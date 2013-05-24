f173cb1c7ea2fe4479af7e6a7048443c1c2483b2
Mark Miyashita <markm@yelp.com>
2013-05-24 16:22:32 -0700

    adding gitconfig stat option

--- zsh/aliases/speed_aliases.zsh
+++ zsh/aliases/speed_aliases.zsh
@@ -2,4 +2,12 @@ alias aliases="cd ~/.yadr/zsh/aliases"
 alias y="cd ~/.yadr"
 alias yu="cd ~/.yadr && git pull && cd - && ar"
 
+function ypush() {
+  echo "$1";
+  cd ~/.yadr;
+  git add .;
+  git commit -m "$1";
+  git push;
+  cd -;
+}
 
