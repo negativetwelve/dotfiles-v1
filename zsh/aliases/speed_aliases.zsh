b71d2ea11697350a94984e36dce26183e1426d00
Mark Miyashita <markm@yelp.com>
2013-08-13 15:39:57 -0700

    more speed aliases

--- zsh/aliases/speed_aliases.zsh
+++ zsh/aliases/speed_aliases.zsh
@@ -19,3 +19,13 @@ if [ `uname` '==' "Linux" ]; then
   alias ls="ls --color=auto"
   alias tmux="$HOME/local/bin/tmux"
 fi
+
+
+alias run-batch="rm ~/logs/test_step2; rm ~/pg/flow_analysis/flow_analysis/data/sign_up_flow_test.json; python batch/adhoc/mobile/mobile_session_analysis/mr_mobile_session_generic_data_with_dimensions.py ~/logs/small_1000000_2_analysis_base > ~/logs/test_step2; cat ~/logs/test_step2 | python batch/adhoc/mobile/mobile_session_analysis/mobile_session_pprint.py --use-stdin > ~/pg/flow_analysis/flow_analysis/data/sign_up_flow_test.json"
+
+alias run-batch-orig="rm ~/logs/test_step2_1; rm ~/pg/flow_analysis/flow_analysis/data/sign_up_flow_test2.json; python batch/adhoc/mobile/mobile_session_analysis/mr_mobile_session_generic_data_with_dimensions.py ~/logs/small_1000000_2_analysis_base > ~/logs/test_step2_1; cat ~/logs/test_step2_1 | python batch/adhoc/mobile/mobile_session_analysis/mobile_session_pprint.py --use-stdin > ~/pg/flow_analysis/flow_analysis/data/sign_up_flow_test2.json"
+
+alias run-debug="python batch/adhoc/mobile/mobile_session_analysis/mr_mobile_session_generic_data_with_dimensions.py ~/logs/small_1000000_2_analysis_base"
+
+
+alias tail_aa="tools/scribereader -e dev api_analytics --tail | ~/lib/jsonq/jsonq '.analytics[*].iri'"
