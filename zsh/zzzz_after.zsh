40b92924b1dd1c84a0e337cc94e8072287633d7f
Mark Miyashita <markm@yelp.com>
2014-02-07 13:09:15 -0800

    Fixing ssh issues

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -17,26 +17,3 @@ fi
 source ~/.yadr/zsh/zsh-func.zsh
 
 PATH=$PATH:/usr/local/share/npm/bin
-
-# Set up ssh-agent
-SSH_ENV="$HOME/.ssh/environment"
-
-function start_agent {
-    echo "Initializing new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
-    chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
-}
-
-# Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
-    }
-else
-    start_agent;
-fi
