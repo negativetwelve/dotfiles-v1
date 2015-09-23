82e78572f27e45fb458029962c6b7cb23b5787cc
Mark Miyashita <negativetwelve@gmail.com>
2015-09-23 02:09:28 -0700

    Remove old aliases so that I can use flow properly

--- zsh/aliases/directory_aliases.zsh
+++ zsh/aliases/directory_aliases.zsh
@@ -1,14 +1,6 @@
 alias projects="cd ~/dropbox/projects"
-
-alias berkeley="cd ~/dropbox/berkeley"
-alias sp13="cd ~/dropbox/berkeley/sp13"
-
 alias cs61a="cd ~/dropbox/projects/cs61a"
-
 alias mmcom="cd ~/dropbox/projects/markmiyashita.com"
-alias tmp="cd ~/tmp"
 alias ios="cd ~/dropbox/projects/ios-pet-game"
-
 alias bp="cd ~/dropbox/projects/blueprint"
 
-alias bench="cd ~/dropbox/projects/bench"
--- zsh/aliases/yelp_aliases.zsh
+++
@@ -1,8 +0,0 @@
-alias yelp="cd ~/pg/yelp-main"
-alias flow="cd ~/pg/flow_analysis"
-alias ytest="cd ~/pg/yelp-main/yelp/tests"
-alias api="cd ~/pg/yelp-main/api_cmds"
-alias api-test="cd ~/pg/yelp-main/yelp/tests/api_cmds"
-
-alias dev3b="ssh -A dev3-devb"
-
